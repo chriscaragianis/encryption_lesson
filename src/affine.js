f84af00ac96ba3d249574ddbdcf7245e8aaf3115
Chris Caragianis <chriscaragianis@gmail.com>
2015-06-26 19:56:59 -0400

    Affine passes spec

--- src/affine.js
+++ src/affine.js
@@ -1,24 +1,30 @@
 function Affine(a, b) {
   if (euclidean_gcd(a, 96) != 1)
-    throw "Bad Argument"
+    throw "Bad Argument";
   this.a = a;
   this.b = b;
- 
+  var a_1 = inverses[mod(a, 96)];
   this.transform = function(input) {
-    return mod((a*input + b), 96);
+    return mod((a*input + b), 96) + 32;
   };
-  
+
   this.invert = function(input) {
+    return mod(a_1*(input - b), 96) + 32;
   };
 
   this.cipher = function(word) {
     var result = '';
     for (var v of word) {
-      result += String.fromCharCode(this.transform(v.charCodeAt(0)) + 32);
+      result += String.fromCharCode(this.transform(v.charCodeAt(0)));
     }
     return result;
   };
-   
+
   this.decipher = function(word) {
+    var result = '';
+    for (var v of word) {
+      result += String.fromCharCode(this.invert(v.charCodeAt(0)));
+    }
+    return result;
   };
 }
