b594168594aa177af1ada4b61dded702991c3894
Chris Caragianis <chriscaragianis@gmail.com>
2015-10-18 13:54:22 -0400

    Set up for letter typing

---
+++ src/letterTyper.js
@@ -0,0 +1,20 @@
+var LetterTyper = class {
+
+  //options {
+  //
+  //  text: the text to be typed
+  //  speed: ms per letter
+  //  blink: (boolean) include a blinking cursor
+  //  blinkColor: (string) color of cursor
+  //  target: JQuery object to fill with text
+  //
+  //}
+  constructor(options) {
+    this.options = options;
+  }
+
+  start() {
+  }
+
+}
+
